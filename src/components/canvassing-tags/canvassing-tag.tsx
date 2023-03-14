a9b5aa558995b37bfb3f5937bfc442bc7d6d867e
maans-dev <hprademeyer123@gmail.com>
2023-03-14 16:17:58 +0000

    test: canvass-tags components

--- src/components/canvassing-tags/canvassing-tag.tsx
+++ src/components/canvassing-tags/canvassing-tag.tsx
@@ -1,24 +1,33 @@
-import { FunctionComponent, useState } from 'react';
-import { EuiCheckableCard, useGeneratedHtmlId } from '@elastic/eui';
-import { ITag } from './types';
+// import { FunctionComponent, useState, useEffect } from 'react';
+// import { EuiCheckableCard, useGeneratedHtmlId } from '@elastic/eui';
+// import { ITags } from './types';
 
-export type Props = {
-  tag: ITag;
-};
+// export type Props = {
+//   tag: ITags;
+//   selected: boolean;
+//   onChange?: () => void;
+// };
 
-const CanvassingTag: FunctionComponent<Props> = ({ tag }) => {
-  const [isSelected, setIsSelected] = useState(false);
-  const checkboxCardId = useGeneratedHtmlId({ prefix: 'checkboxCard' });
+// const CanvassingTag: FunctionComponent<Props> = ({
+//   tag,
+//   selected,
+//   onChange,
+// }) => {
+//   const [isSelected, setIsSelected] = useState(selected);
+//   const checkboxCardId = useGeneratedHtmlId({ prefix: 'checkboxCard' });
 
-  return (
-    <EuiCheckableCard
-      id={checkboxCardId}
-      label={tag.description}
-      checkableType="checkbox"
-      checked={isSelected}
-      onChange={() => setIsSelected(!isSelected)}
-    />
-  );
-};
+//   return (
+//     <EuiCheckableCard
+//       id={checkboxCardId}
+//       label={tag.description}
+//       checkableType="checkbox"
+//       checked={isSelected}
+//       onChange={() => {
+//         setIsSelected(!isSelected);
+//         onChange();
+//       }}
+//     />
+//   );
+// };
 
-export default CanvassingTag;
+// export default CanvassingTag;
--- src/components/canvassing-tags/index.tsx
+++ src/components/canvassing-tags/index.tsx
@@ -1,30 +1,94 @@
-import { FunctionComponent } from 'react';
-import { EuiFlexGrid, EuiFlexItem, useIsWithinBreakpoints } from '@elastic/eui';
-import { ITag } from './types';
-import CanvassingTag from './canvassing-tag';
+// import { FunctionComponent } from 'react';
+// import { EuiFlexGrid, EuiFlexItem, useIsWithinBreakpoints } from '@elastic/eui';
+// import { ITags } from './types';
+// import CanvassingTag from './canvassing-tag';
 
-export type Props = {
-  tags: ITag[];
-};
+// export type Props = {
+//   fields: ITags;
+//   onTagClick?: (tag: ITags) => void;
+// };
 
-const CanvassingTags: FunctionComponent<Props> = ({ tags }) => {
-  const isMobile = useIsWithinBreakpoints(['xs', 's']);
+// const presetTags: ITags = {
+//   fields: [
+//     {
+//       field: {
+//         category: 'Canvassing',
+//         code: 'WR',
+//         description: 'Will Register',
+//         active: false,
+//       },
+//     },
+//     {
+//       field: {
+//         category: 'Canvassing',
+//         code: 'ASTREG',
+//         description: 'Assisted to register',
+//         active: false,
+//       },
+//     },
+//     {
+//       field: {
+//         category: 'Canvassing',
+//         code: 'DR',
+//         description: 'Did register',
+//         active: false,
+//       },
+//     },
+//     {
+//       field: {
+//         category: 'Canvassing',
+//         code: 'WV',
+//         description: "Won't vote",
+//         active: false,
+//       },
+//     },
+//     {
+//       field: {
+//         category: 'Canvassing',
+//         code: 'CV',
+//         description: "Can't vote",
+//         active: false,
+//       },
+//     },
+//     {
+//       field: {
+//         category: 'Canvassing',
+//         code: 'M',
+//         description: 'Moved',
+//         active: false,
+//       },
+//     },
+//   ],
+// };
 
-  return (
-    <EuiFlexGrid
-      columns={isMobile ? 1 : 3}
-      direction="row"
-      gutterSize="s"
-      responsive={true}>
-      {tags?.map((item: ITag, i) => {
-        return (
-          <EuiFlexItem key={i} grow={false} style={{ minWidth: 100 }}>
-            <CanvassingTag tag={item} />
-          </EuiFlexItem>
-        );
-      })}
-    </EuiFlexGrid>
-  );
-};
+// const CanvassingTags: FunctionComponent<Props> = ({ fields, onTagClick }) => {
+//   const isMobile = useIsWithinBreakpoints(['xs', 's']);
 
-export default CanvassingTags;
+//   // extract the fields array from the fields object
+//   const tagFields = fields?.fields || [];
+
+//   return (
+//     <EuiFlexGrid
+//       columns={isMobile ? 1 : 3}
+//       direction="row"
+//       gutterSize="s"
+//       responsive={true}>
+//       {tagFields.map((tag: ITags, i) => {
+//         return (
+//           <EuiFlexItem key={i} grow={false} style={{ minWidth: 100 }}>
+//             <CanvassingTag
+//               tag={tag}
+//               selected={tag.active}
+//               onChange={() => {
+//                 const updatedTag = { ...tag, enabled: !tag.enabled };
+//                 onTagClick && onTagClick(updatedTag);
+//               }}
+//             />
+//           </EuiFlexItem>
+//         );
+//       })}
+//     </EuiFlexGrid>
+//   );
+// };
+
+// export default CanvassingTags;
