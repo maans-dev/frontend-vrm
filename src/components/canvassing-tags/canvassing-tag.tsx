e8fca9efedf223138fe36f670424d19f3a42e489
Herman Rademeyer <hr@bizvoip.co.za>
2023-05-23 08:12:29 +0000

    Chore/disabled did register

--- src/components/canvassing-tags/canvassing-tag.tsx
+++ src/components/canvassing-tags/canvassing-tag.tsx
@@ -2,6 +2,9 @@ import { FunctionComponent, useEffect, useState } from 'react';
 import { EuiCheckableCard, useGeneratedHtmlId } from '@elastic/eui';
 import { Field } from '@lib/domain/person';
 import { useCanvassFormReset } from '@lib/hooks/use-canvass-form-reset';
+import { hasRole as hasRoleUtil } from '@lib/auth/utils';
+import { Roles } from '@lib/domain/auth';
+import { useSession } from 'next-auth/react';
 
 export type Props = {
   field: Partial<Field>;
@@ -11,6 +14,8 @@ export type Props = {
 const CanvassingTag: FunctionComponent<Props> = ({ field, onChange }) => {
   const [isSelected, setIsSelected] = useState<boolean>(field.value as boolean);
   const checkboxCardId = useGeneratedHtmlId({ prefix: 'checkboxCard' });
+  const { data: session } = useSession();
+  const hasRole = (role: string) => hasRoleUtil(role, session?.user?.roles);
 
   useCanvassFormReset(() => {
     setIsSelected(field.value as boolean);
@@ -26,6 +31,7 @@ const CanvassingTag: FunctionComponent<Props> = ({ field, onChange }) => {
       label={field?.field?.name}
       checkableType="checkbox"
       checked={isSelected}
+      disabled={field.field?.readOnly && !hasRole(Roles.SuperUser)}
       onChange={() => {
         setIsSelected(!isSelected);
         onChange({ ...field, value: !isSelected });
