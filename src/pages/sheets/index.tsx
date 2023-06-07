a3ce62800b89e29a13f6817223cd6f5fab502985
maans-dev <hprademeyer123@gmail.com>
2023-06-07 12:10:26 +0000

    fix: home breadcrumb on sheet gen page

--- src/pages/sheets/index.tsx
+++ src/pages/sheets/index.tsx
@@ -2,9 +2,19 @@ import { FunctionComponent } from 'react';
 import { EuiBreadcrumb } from '@elastic/eui';
 import MainLayout from '@layouts/main';
 import PagePlaceholder from '@components/page-placeholder';
+import { useRouter } from 'next/router';
 
 const Index: FunctionComponent = () => {
+  const router = useRouter();
   const breadcrumb: EuiBreadcrumb[] = [
+    {
+      text: 'Home',
+      href: '/',
+      onClick: e => {
+        router.push('/');
+        e.preventDefault();
+      },
+    },
     {
       text: 'Generate Sheets',
     },
